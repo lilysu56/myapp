b17c6f0a85e5672935bcffa4c94e46c1ef2ec6f0
lilysu56 <slj9359@gmail.com>
2026-03-04 14:29:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/myapp/', // 這行最重要，對應你的倉庫名稱
+  // 這是為了讓你的 App 在 GitHub Pages 也能正確讀取資源
+  base: '/myapp/',
+  
+  // 這裡是重點：告訴 Vite 當它看到 '@' 時，代表從「專案根目錄」開始找
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./', import.meta.url))
+    }
+  }
 })
